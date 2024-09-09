6eeb5782e636574c1aec1a6b579291b44f70018e
start.vaadin.com <no-reply@vaadin.com>
2024-09-09 16:24:25 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/send/SendView.java
@@ -0,0 +1,36 @@
+package com.example.application.views.send;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Send")
+@Menu(icon = "line-awesome/svg/address-book-solid.svg", order = 0)
+@Route(value = "")
+@RouteAlias(value = "")
+public class SendView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public SendView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
